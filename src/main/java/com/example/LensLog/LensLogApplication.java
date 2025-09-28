9b2fd54c08ba23ec99e2afc8aef546b58a7e0121
fish-minkyu <e951219@naver.com>
2025-09-28 21:11:31 +0900

    Add Project init

---
+++ src/main/java/com/example/LensLog/LensLogApplication.java
@@ -0,0 +1,13 @@
+package com.example.LensLog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LensLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LensLogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/LensLog/LensLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LensLog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LensLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
