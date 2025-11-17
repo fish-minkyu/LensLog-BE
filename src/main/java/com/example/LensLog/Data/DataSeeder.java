7365ac9aa395283649ef7d048f39c44de4eb469c
fish-minkyu <e951219@naver.com>
2025-11-17 18:47:32 +0900

    Feat. category mock data 생성

--- src/main/java/com/example/LensLog/Data/DataSeeder.java
+++ src/main/java/com/example/LensLog/Data/DataSeeder.java
@@ -2,6 +2,8 @@ package com.example.LensLog.Data;
 
 import com.example.LensLog.auth.entity.User;
 import com.example.LensLog.auth.repo.UserRepository;
+import com.example.LensLog.category.entity.Category;
+import com.example.LensLog.category.repo.CategoryRepository;
 import com.example.LensLog.constant.LoginTypeConstant;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -10,6 +12,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 @Slf4j
 @Component
@@ -17,6 +22,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class DataSeeder implements CommandLineRunner {
     private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
+    private final CategoryRepository categoryRepository;
     private static final String ADMIN_EMAIL = "e951219@naver.com";
     private static final String PASSWORD = "lensLog123!";
 
@@ -31,21 +37,42 @@ public class DataSeeder implements CommandLineRunner {
         }
 
         try {
-            User admin = User.builder()
-                .username("e951219")
-                .password(passwordEncoder.encode(PASSWORD))
-                .name("관리자")
-                .email(ADMIN_EMAIL)
-                .isVerified(true)
-                .provider(LoginTypeConstant.LOCAL)
-                .authority("ROLE_ADMIN")
-                .build();
+            makeAdmin();
+            makeCategory();
 
-            userRepository.save(admin);
             System.out.println("DataSeeder activation success!!");
         } catch (Exception e) {
             log.error("DataSeeder activation has a error: {}", e.getMessage());
             throw e;
         }
+    }
+
+    public void makeAdmin() {
+        User admin = User.builder()
+            .username("e951219")
+            .password(passwordEncoder.encode(PASSWORD))
+            .name("관리자")
+            .email(ADMIN_EMAIL)
+            .isVerified(true)
+            .provider(LoginTypeConstant.LOCAL)
+            .authority("ROLE_ADMIN")
+            .build();
+
+        userRepository.save(admin);
+    }
+
+    public void makeCategory() {
+        List<Category> params = new ArrayList<>();
+
+        List<String> titles = List.of("제주도", "경주", "홍콩");
+        for (String title : titles) {
+            Category newCategory = Category.builder()
+                .categoryName(title)
+                .build();
+
+            params.add(newCategory);
+        }
+
+        categoryRepository.saveAll(params);
     }
 }
